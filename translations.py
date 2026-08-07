1e2642d6ecd22eac65b8c215f8a4520f1f36c23b
thehskisgood <hank29.ye@gmail.com>
2026-08-07 17:29:22 +0800

    更新 translations.py

--- translations.py
+++ translations.py
@@ -74,11 +74,11 @@ MAP_TRANSLATION = {
     "Sunset": "日落之城",
     "Abyss": "深窟幽境",
     "Corrode": "晶蝕之地",
-    "Shirmish A" : "火線交鋒A",
-    "Shirmish B" : "火線交鋒B",
-    "Shirmish C" : "火線交鋒C",
-    "Shirmish D" : "火線交鋒D",
-    "Shirmish E" : "火線交鋒E",
+    "Skirmish A" : "火線交鋒A",
+    "Skirmish B" : "火線交鋒B",
+    "Skirmish C" : "火線交鋒C",
+    "Skirmish D" : "火線交鋒D",
+    "Skirmish E" : "火線交鋒E",
 }
 
 
